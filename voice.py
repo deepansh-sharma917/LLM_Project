06765b41d955b91bcd7e7a2241f34f64d89302af
deepansh-sharma917 <49142993+deepansh-sharma917@users.noreply.github.com>
2025-06-02 20:12:26 +0530

    Voice ffile speech recognition

---
+++ voice.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr
+import pyttsx3
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def listen():
+    with sr.Microphone() as source:
+        print("🎤 Listening...")
+        audio = recognizer.listen(source)
+    try:
+        return recognizer.recognize_google(audio)
+    except:
+        return "Sorry, I didn't catch that."
